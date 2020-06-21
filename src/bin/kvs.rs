7edf9c130c0d2ef3d5aac26cefb476fe0ee039b4
Nick Calibey <nick.calibey@gmail.com>
2020-06-21 17:13:32 -0500

    Refactor to use structopt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,11 +1,26 @@
-use clap::{App, Arg, SubCommand};
-use std::env;
 use std::process::exit;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "kvs")]
-struct Opt {}
+enum Command {
+    Get {
+        #[structopt()]
+        key: String,
+    },
+    Set {
+        key: String,
+        value: String,
+    },
+    Rm {
+        key: String,
+    },
+}
+
+#[derive(StructOpt, Debug)]
+struct App {
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
+}
 
 fn main() {
     exit(match run_app() {
@@ -15,41 +30,12 @@ fn main() {
 }
 
 fn run_app() -> Result<(), i32> {
-    let matches = App::new("kvs")
-        .version(env::var_os("CARGO_PKG_VERSION").unwrap().to_str().unwrap())
-        .author("Nick Calibey <nick.calibey@gmail.com>")
-        .about("Key-value store")
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set the value of a string key to a string")
-                .arg(Arg::with_name("key").value_name("KEY").required(true))
-                .arg(Arg::with_name("value").value_name("VALUE").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("Get the string value of a given string key")
-                .arg(Arg::with_name("key").value_name("KEY").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("Remove a given key")
-                .arg(Arg::with_name("key").value_name("KEY").required(true)),
-        )
-        .get_matches();
-    if let Some(_) = matches.subcommand_matches("get") {
-        eprintln!("unimplemented");
-        return Err(1);
+    let app = App::from_args();
+    match app.cmd {
+        Some(_) => {
+            eprintln!("unimplemented");
+        }
+        _ => (),
     }
-    if let Some(_) = matches.subcommand_matches("set") {
-        eprintln!("unimplemented");
-        return Err(1);
-    }
-    if let Some(_) = matches.subcommand_matches("rm") {
-        eprintln!("unimplemented");
-        return Err(1);
-    }
-    if matches.args.len() == 0 {
-        return Err(1);
-    }
-    Ok(())
+    Err(1)
 }
